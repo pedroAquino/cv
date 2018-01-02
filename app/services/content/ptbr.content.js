5a83fa109f5ef8868c9466d522590bc80aac0cb5
pedroAquino <pedrohenrique.aquino@live.com>
2018-01-02 17:45:29 -0200

    making the content dynamic

--- app/services/content/ptbr.content.js
+++ app/services/content/ptbr.content.js
@@ -8,10 +8,199 @@ module.exports = {
     ],
     'aboutMe': {
         'title': 'SOBRE MIM',
-        'teste': 'Meu texto e no meio um #[span SPAN]',
-        'visibleContent': `Minha história com o desenvolvimento de software começou relativamente cedo. Com 15 anos fiz meus 
-                          primeiros programas em access (shame !), e daí não parei mais. De lá para cá foram dez longos anos e
-                          uma carreira da qual não me arrependo em, praticamente, nada ! Tenho 25 anos, sou solteiro, moro  em Ribeirão Pires - SP (região metropolitana).`,
-        'hiddenContent': `trabalhei em diversos lugares e aprendi diversas linguagens, mas o amor da minha vida sempre foi javascript. Me considero proficiente em javascript, css, html, c#.net e, nos últimos tempos tenho buscado novos mares, navegando pelas águas desconhecidas do node.js, não preciso dizer que já estou viciado ! Em 2015, tive a oportunidade de passar 6 meses na`
+        'content': `Minha história com o desenvolvimento de software começou relativamente cedo. Com 15 anos fiz meus 
+        primeiros programas em access (shame !), e daí não parei mais. De lá para cá foram dez longos anos e
+        uma carreira da qual não me arrependo em, praticamente, nada ! Tenho 25 anos, sou solteiro, moro  em Ribeirão Pires - SP (região metropolitana).
+        <span class="state-hidden">
+            &nbsp; trabalhei em diversos lugares e aprendi diversas linguagens, mas o amor da minha vida sempre foi javascript.
+            Me considero proficiente em javascript, css, html, c#.net e, nos últimos tempos tenho buscado novos mares, navegando pelas águas desconhecidas do node.js, não preciso dizer que já estou viciado ! 
+            Em 2015, tive a oportunidade de passar 6 meses na <span class="dark bold">irlanda &nbsp;</span>
+            foi uma experiência fantástica, e ainda pude me tornar proficiante em inglês. 
+            ambém sou agilista, fissurado por produtividade. Nas horas vagas eu jogo video-game, leio, viajo e 
+            tento  aprender novas tecnologias.
+        </span>`,
+        'label': 'ver mais'
     },
+    'habilities': {
+        'title': 'HABILIDADES',
+        'content': [
+            {
+                'topic': 'Inglês',
+                'level': '<div class="bar-child advanced"></div>',
+                'levelText': 'avançado'
+            },
+            {
+                'topic': 'Javascript / Node.js',
+                'level': '<div class="bar-child advanced"></div>',
+                'levelText': 'avançado'
+            },
+            {
+                'topic': 'HTML / CSS / Jquery',
+                'level': '<div class="bar-child advanced"></div>',
+                'levelText': 'avançado'
+            },
+            {
+                'topic': 'Compiladores CSS (SASS / Stylus)',
+                'level': '<div class="bar-child basic"></div>',
+                'levelText': 'básico'
+            },
+            {
+                'topic': 'Ionic',
+                'level': '<div class="bar-child intermediate"></div>',
+                'levelText': 'intermediário'
+            },
+            {
+                'topic': 'Angular.js',
+                'level': '<div class="bar-child intermediate"></div>',
+                'levelText': 'intermediário'
+            },
+            {
+                'topic': 'Xamarin',
+                'level': '<div class="bar-child intermediate"></div>',
+                'levelText': 'intermediário'
+            },
+            {
+                'topic': 'C#.NET',
+                'level': '<div class="bar-child advanced"></div>',
+                'levelText': 'avançado'
+            },
+            {
+                'topic': 'DevOps / Powershell',
+                'level': '<div class="bar-child basic"></div>',
+                'levelText': 'básico'
+            },
+        ]
+    },
+    'certifications': {
+        'title': 'CERTIFICAÇÕES',
+        'emitters': [
+            {
+                'name': 'Scrum.org',
+                'certificates': [
+                    {
+                        'title': 'scrum master 1.',
+                        'address': '#'
+                    },
+                    {
+                        'title': 'scrum master 2.',
+                        'address': '#'
+                    }
+                ]
+            },
+            {
+                'name': 'Microsoft (MCSD Web Developer)',
+                'certificates': [
+                    {
+                        'title': '70-480 - Programando em HTML5 e CSS3.',
+                        'address': '#'
+                    },
+                    {
+                        'title': '70-486 - Desenvolvendo aplicações ASP.NET.',
+                        'address': '#'
+                    },
+                    {
+                        'title': '70-487 - Desenvolvendo para Microsoft azure e Web Services.',
+                        'address': '#'
+                    }
+                ]
+            }
+        ]
+    },
+    'experiences': {
+        'title': 'EXPERIÊNCIAS',
+        'workplaces': [
+            {
+                'name': 'BRQ Tecnologia.',
+                'address': 'http://www.brq.com/',
+                'periods': ['novembro / 2016 - atual'],
+                'role': 'desenvolvedor  .NET',
+                'description': `Na BRQ tive a oportunidade de prestar consultoria para o banco Itaú,um dos maiores bancos da américa latina. 
+                Trabalhei diretamente no desenvolvimento do app (android e ios) do banco, utilizando C#.NET (chamado no app nativo como uma web view) 
+                e linguagens de frontend em geral (CSS, HTML, javascript, jquery).`,
+                'deliveries': {
+                    'title': 'principais entregas:',
+                    'items': [
+                        'redesign do resgate de poupança do app Itaú.'
+                    ]
+                }
+            },
+            {
+                'name': 'Geosiga, soluções em geopos.',
+                'address': 'https://www.geosiga.com.br/',
+                'periods': [
+                    'novembro / 2014 - novembro / 2015',
+                    'julho / 2012 - julho / 2013'
+                ],
+                'role': 'analista programador .NET',
+                'description': `Na Geosiga tive a oportunidade de trabalhar por duas vezes. Na minha primeira passagem, sendo meu primeiro emprego na área de desenvolvimento, tive a oportunidade de aprender muito e também contribuir com algumas entregas veliosas para a empresa. Na segunda oportunidade, pude contribuir com um maior conhecimento técnico que havia adquirido, além da melhoria dos processos de desenvolvimento da empresa.`,
+                'deliveries': {
+                    'title': 'principais entregas:',
+                    'items': [
+                        'envio de bloqueio para veículos rastreados pelo sistema em tempo real',
+                        'integração do software de helpdesk (kayako) com os sistemas internos da empresa',
+                        'ambiente de integração contínua',
+                        'construção de api para o produto da empresa, possibilitando a integração com outros sistemas',
+                        'implementação do processo de desenvolvimento scrum',
+                        'contratação de desenvolvedores e montagem de time.'
+                    ]
+                }
+            },
+            {
+                'name': 'Next Soft Tecnologia.',
+                'address': 'http://www.nexttecnologia.com.br/site/home.aspx',
+                'periods': [
+                    'agosto / 2014 - novembro / 2014',
+                ],
+                'role': 'analista programador .NET',
+                'description': `Na next soft, apesar do pouco tempo, pude adquirir conhecimentos sobre desenvolvimento desktop trabalhando no software de PDV da empresa, além de automatizar os testes do sistema.`,
+                'deliveries': {
+                    'title': 'principais entregas:',
+                    'items': [
+                        'correção de bug na comunicação do PDV (desktop) com a impressora fiscal epson',
+                        'automatização de testes'
+                    ]
+                }
+            },
+            {
+                'name': 'OOHM Tecnologia.',
+                'address': 'http://www.oohm.com/',
+                'periods': [
+                    'julho / 2013 - julho / 2014',
+                ],
+                'role': 'programador .NET, scrum master',
+                'description': `A OOHM é a empresa que considero que tive meus maiores aprendizados, pude aprender muito, não só sobre programação mas também sobre metodologias ágeis, teste de software, DDD e etc.`,
+                'deliveries': {
+                    'title': 'principais entregas:',
+                    'items': [
+                        'pagamento com paypall (e-commerce)',
+                        'automatização de testes em PHP',
+                        'condução de reuniões e contato direto com cliente.'
+                    ]
+                }
+            }
+        ]
+    },
+    'education': {
+        'title': 'EDUCAÇÃO',
+        'courses': [
+            {
+                'name': 'informática para negócios',
+                'institution': 'Fatec Mauá',
+                'address': 'http://www.fatecmaua.com.br/',
+                'status': 'concluído, 2017',
+            },
+            {
+                'name': 'informática para internet',
+                'institution': 'Etec Lauro Gomes',
+                'address': 'http://www.etelg.com.br/',
+                'status': 'concluído, 2012',
+            },
+            {
+                'name': 'eletricista de manutenção',
+                'institution': 'Senai A. Jacob Lafer',
+                'address': 'https://santoandre.sp.senai.br/',
+                'status': 'concluído, 2008',
+            }
+        ]
+    }
 };
