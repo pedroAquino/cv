afad7ef098070866354c9e6f3395081e5a26e74e
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-01-24 19:08:27 -0200

    testing languages

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express'),
 	router = express.Router(),
+	accepts = require('accepts'),
 	ContentService = require('../services/content/content.service');
 
 router.get('/', function(request, response, next) {
 	var contentService = new ContentService();
 	// response.render('index', contentService.getContent('pt-BR'));
+	console.log('LANGUAGES  :');
+	console.log(accepts(request).languages());
 	response.render('index', contentService.getContent('en-US'));
 });
 
