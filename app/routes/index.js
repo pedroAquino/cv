4e8f630a30b9ac0f3a33b9a9f7689ca0c18ba44b
pedroAquino <pedrohenrique.aquino@live.com>
2018-01-25 17:38:25 -0200

    creating routes for each language

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,9 +4,24 @@ var express = require('express'),
 	ContentService = require('../services/content/content.service');
 
 router.get('/', function(request, response, next) {
+	var language = accepts(request).languages()[0].toLowerCase();
+
+	if (language === 'pt-br' ||
+		language === 'pt') {
+		sresponse.redirect('/pt');
+	} else {
+		response.redirect('/en');
+	}
+});
+
+router.get('/pt', function(request, response, next) {
 	var contentService = new ContentService();
-	var language = accepts(request).languages()[0];
-	response.render('index', contentService.getContent(language));
+	response.render('index', contentService.getContent('pt'));
+});
+
+router.get('/en', function(request, response, next) {
+	var contentService = new ContentService();
+	response.render('index', contentService.getContent('en'));
 });
 
 module.exports = router;
--- app/services/content/content.service.js
+++ app/services/content/content.service.js
@@ -4,12 +4,9 @@ var contentEn = require('./en.content');
 module.exports = function ContentService() {
     
     this.getContent = function(language) {
+        language = language !== 'pt' && 
+            language !== 'pt-br' ? 'en' : 'pt';
 
-        if (language === 'pt-BR' ||
-            language == 'pt') {
-            return contentPt;
-        } else {
-            return contentEn;
-        }
+        return require(`./${language}.content`);
     }
 }
