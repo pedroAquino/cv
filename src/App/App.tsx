afbcff0ffdf23e18e795cd12945ca679c846fa72
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-02-14 14:36:23 -0200

    adding headings

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -8,12 +8,12 @@ class App extends Component {
     return (
       <main>
         <h1>Heading 1</h1>
-        <h2>Heading 1</h2>
-        <h3>Heading 1</h3>
-        <p>Heading 1</p>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <p>Normal Text</p>
       </main>
     );
   }
 }
 
-export default App;
+export default injectSheet(globalStyles)(App);
