c6d19f46a67e9361642508444b5d83eb9f512c27
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-02-14 14:24:18 -0200

    adding global styles

---
+++ src/App/App.styles.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  spacing: {
+      unit: 4
+  }   
+};
+
+export const globalStyles = {
+    '@global': {
+        body: {
+            fontFamily: 'Open Sans, sans-serif',
+            color: '#9099a0',
+            fontSize: 16,
+            lineHeight: '1.5em'
+        },
+        h1: {
+            color: '#3d3e42',
+            fontSize: '2em'
+        },
+        h2: {
+            color: '#6c9',
+            fontSize: '1.3em'
+        },
+        h3: {
+            color: '#eee',
+            fontSize: 16
+        }
+    }
+};
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import injectSheet, {ThemeProvider} from 'react-jss';
+import { theme, globalStyles } from './App.styles';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <main>
+        <h1>Heading 1</h1>
+        <h2>Heading 1</h2>
+        <h3>Heading 1</h3>
+        <p>Heading 1</p>
       </main>
     );
   }
